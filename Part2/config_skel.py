ebd656674440a3705b002303229c08df152d0ea6
Ian Smith <ian_smith_12a@mymts.net>
2016-09-03 19:16:42 -0500

    Initial commit

---
+++ Part2/config_skel.py
@@ -0,0 +1,7 @@
+# This file contains the username and password to your bot.
+# DO NOT check it in!
+
+# Fill in the values and rename this file to config_bot.py
+
+REDDIT_USERNAME = 'sniperbot12a'  # YOUR USERNAME as string
+REDDIT_PASS = 'car12black'  # YOUR PASSWORD as string
