5593d9d2a2fbad13afbff3ebb43d03151efbf205
Ian Smith <ian_smith_12a@mymts.net>
2016-09-03 20:47:44 -0500

    Restarting

--- Part2/config_skel.py
+++
@@ -1,7 +0,0 @@
-# This file contains the username and password to your bot.
-# DO NOT check it in!
-
-# Fill in the values and rename this file to config_bot.py
-
-REDDIT_USERNAME = 'sniperbot12a'  # YOUR USERNAME as string
-REDDIT_PASS = 'car12black'  # YOUR PASSWORD as string
